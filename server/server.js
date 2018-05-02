50c695064c3ddbd4effd2fa288e7ba294451b093
Munkhtegsh Munkhbat <munkhtegsh.m@gmail.com>
2018-05-02 14:09:31 -0600

    Added server

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-app.use(bodyParser.json());
-
+app.use(express.static(`${__dirname}./../public`));
 const port = 4000;
-app.listen(port, () => console.log(`Server is running up on: ${app}`))
+app.listen(port, () => console.log(`Server is running up on: ${port}`))
